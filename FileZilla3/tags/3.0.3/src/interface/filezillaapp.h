6f29c044ff58d7d3ff17fe0edcfccae0a1e565f8
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2007-11-07 09:39:00 +0000

    Tag 3.0.3

---
+++ FileZilla3/tags/3.0.3/src/interface/filezillaapp.h
@@ -0,0 +1,63 @@
+#ifndef __FILEZILLAAPP_H__
+#define __FILEZILLAAPP_H__
+
+#if wxUSE_DEBUGREPORT && wxUSE_ON_FATAL_EXCEPTION
+#include <wx/debugrpt.h>
+#endif
+
+class CWrapEngine;
+class CFileZillaApp : public wxApp
+{
+public:
+	CFileZillaApp();
+	virtual ~CFileZillaApp();
+
+	virtual bool OnInit();
+	
+	wxString GetResourceDir() const { return m_resourceDir; }
+	wxString GetSettingsDir() const { return m_settingsDir; }
+	wxString GetDefaultsDir() const { return m_defaultsDir; }
+	wxString GetLocalesDir() const { return m_localesDir; }
+
+	void CheckExistsFzsftp();
+
+	bool SetLocale(int language);
+	int GetCurrentLanguage() const;
+
+	void DisplayEncodingWarning();
+
+	CWrapEngine* GetWrapEngine();
+
+protected:
+	bool InitDefaultsDir();
+	bool InitSettingsDir();
+	wxString GetSettingsDirFromDefaults();
+	bool LoadResourceFiles();
+	bool LoadLocales();
+
+	wxLocale* m_pLocale;
+
+	wxString m_resourceDir;
+	wxString m_defaultsDir;
+	wxString m_settingsDir;
+	wxString m_localesDir;
+
+#if wxUSE_DEBUGREPORT && wxUSE_ON_FATAL_EXCEPTION
+	virtual void OnFatalException();
+	void GenerateReport(wxDebugReport::Context ctx);
+#endif
+
+	wxString GetDataDir(wxString fileToFind) const;
+
+	// FileExists acceppts full paths as parameter,
+	// with the addition that path segments may be obmitted
+	// with a wildcard (*). A matching directory will then be searched.
+	// Example: FileExists(_T("/home/*/.filezilla/filezilla.xml"));
+	bool FileExists(const wxString& file) const;
+
+	CWrapEngine* m_pWrapEngine;
+};
+
+DECLARE_APP(CFileZillaApp)
+
+#endif //__FILEZILLAAPP_H__
---
+++ FileZilla3/tags/3.0.3/src/interface/statuslinectrl.h
@@ -0,0 +1,46 @@
+#ifndef __STATUSLINECTRL_H__
+#define __STATUSLINECTRL_H__
+
+class CQueueView;
+class CStatusLineCtrl : public wxWindow
+{
+public:
+	CStatusLineCtrl(CQueueView* pParent, const t_EngineData* const pEngineData, const wxRect& initialPosition);
+	~CStatusLineCtrl();
+
+	const CFileItem* const GetItem() const { return m_pEngineData->pItem; }
+
+	void SetTransferStatus(const CTransferStatus* pStatus);
+	wxLongLong GetLastOffset() const { return m_pStatus ? m_pStatus->currentOffset : m_lastOffset; }
+	wxLongLong GetTotalSize() const { return m_pStatus ? m_pStatus->totalSize : -1; }
+	wxLongLong GetSpeed() const;
+
+	// Returns true if some progress has been made since transfer started
+	bool MadeProgress() const { return m_madeProgress; }
+
+protected:
+	void DrawRightAlignedText(wxDC& dc, wxString text, int x, int y);
+	void DrawProgressBar(wxDC& dc, int x, int y, int height);
+
+	CQueueView* m_pParent;
+	const t_EngineData* const m_pEngineData;
+	CTransferStatus* m_pStatus;
+
+	wxString m_statusText;
+	wxTimer m_transferStatusTimer;
+
+	static int m_fieldOffsets[4];
+	static wxCoord m_textHeight;
+	static bool m_initialized;
+
+	bool m_madeProgress;
+
+	wxLongLong m_lastOffset; // Stores the last transfer offset so that the total queue size can be accurately calculated.
+
+	DECLARE_EVENT_TABLE()
+	void OnPaint(wxPaintEvent& event);
+	void OnTimer(wxTimerEvent& event);
+	void OnEraseBackground(wxEraseEvent& event);
+};
+
+#endif // __STATUSLINECTRL_H__

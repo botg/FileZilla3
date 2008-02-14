cac870b3c7078a96d2bcd7a48c24c61d722acd46
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2008-02-14 00:23:46 +0000

    made a copy

---
+++ FileZilla3/tags/3.0.7/src/interface/listctrlex.cpp
@@ -0,0 +1,219 @@
+#include "FileZilla.h"
+#include "listctrlex.h"
+
+DECLARE_EVENT_TYPE(fzEVT_POSTSCROLL, -1)
+DEFINE_EVENT_TYPE(fzEVT_POSTSCROLL)
+
+BEGIN_EVENT_TABLE(wxListCtrlEx, wxListCtrl)
+EVT_COMMAND(wxID_ANY, fzEVT_POSTSCROLL, wxListCtrlEx::OnPostScrollEvent)
+EVT_SCROLLWIN(wxListCtrlEx::OnScrollEvent)
+EVT_MOUSEWHEEL(wxListCtrlEx::OnMouseWheel)
+EVT_LIST_ITEM_FOCUSED(wxID_ANY, wxListCtrlEx::OnSelectionChanged)
+EVT_LIST_ITEM_SELECTED(wxID_ANY, wxListCtrlEx::OnSelectionChanged)
+EVT_KEY_DOWN(wxListCtrlEx::OnKeyDown)
+END_EVENT_TABLE()
+
+wxListCtrlEx::wxListCtrlEx(wxWindow *parent,
+						   wxWindowID id,
+						   const wxPoint& pos,
+						   const wxSize& size,
+						   long style,
+						   const wxValidator& validator,
+						   const wxString& name)
+						   : wxListCtrl(parent, id, pos, size, style, validator, name)
+{
+	m_prefixSearch_enabled = false;
+}
+
+wxListCtrlEx::~wxListCtrlEx()
+{
+}
+
+#ifndef __WXMSW__
+wxScrolledWindow* wxListCtrlEx::GetMainWindow()
+{
+#ifdef __WXMAC__
+	return (wxScrolledWindow*)m_genericImpl->m_mainWin;
+#else
+	return (wxScrolledWindow*)m_mainWin;
+#endif
+}
+#endif
+
+void wxListCtrlEx::OnPostScroll()
+{
+}
+
+void wxListCtrlEx::OnPostScrollEvent(wxCommandEvent& event)
+{
+	OnPostScroll();
+}
+
+void wxListCtrlEx::OnPreEmitPostScrollEvent()
+{
+	EmitPostScrollEvent();
+}
+
+void wxListCtrlEx::EmitPostScrollEvent()
+{
+	wxCommandEvent evt(fzEVT_POSTSCROLL, wxID_ANY);
+	AddPendingEvent(evt);
+}
+
+void wxListCtrlEx::OnScrollEvent(wxScrollWinEvent& event)
+{
+	event.Skip();
+	OnPreEmitPostScrollEvent();
+}
+
+void wxListCtrlEx::OnMouseWheel(wxMouseEvent& event)
+{
+	event.Skip();
+	OnPreEmitPostScrollEvent();
+}
+
+void wxListCtrlEx::OnSelectionChanged(wxListEvent& event)
+{
+	event.Skip();
+	OnPreEmitPostScrollEvent();
+}
+
+void wxListCtrlEx::ScrollTopItem(int item)
+{
+#ifdef __WXMSW__
+	const int current = GetTopItem();
+
+	int delta = item - current;
+	if (!delta)
+		return;
+
+	wxRect rect;
+	GetItemRect(current, rect, wxLIST_RECT_BOUNDS);
+
+	delta *= rect.GetHeight();
+	ScrollList(0, delta);
+#else
+	GetMainWindow()->Scroll(0, item);
+	EnsureVisible(item);
+#endif
+}
+
+
+void wxListCtrlEx::HandlePrefixSearch(wxChar character)
+{
+	wxASSERT(character);
+
+	// Keyboard navigation within items
+	wxDateTime now = wxDateTime::UNow();
+	if (m_prefixSearch_lastKeyPress.IsValid())
+	{
+		wxTimeSpan span = now - m_prefixSearch_lastKeyPress;
+		if (span.GetSeconds() >= 1)
+			m_prefixSearch_prefix = _T("");
+	}
+	m_prefixSearch_lastKeyPress = now;
+
+	wxString newPrefix = m_prefixSearch_prefix + character;
+
+	bool beep = false;
+	int item = GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED);
+	if (item != -1)
+	{
+		wxString text = GetItemText(item, 0);
+		if (text.Length() >= m_prefixSearch_prefix.Length() && !m_prefixSearch_prefix.CmpNoCase(text.Left(m_prefixSearch_prefix.Length())))
+			beep = true;
+	}
+	else if (m_prefixSearch_prefix == _T(""))
+		beep = true;
+
+	int start = item;
+	if (start < 0)
+		start = 0;
+
+	int newPos = FindItemWithPrefix(newPrefix, start);
+
+	if (newPos == -1 && m_prefixSearch_prefix[0] == character && !m_prefixSearch_prefix[1] && item != -1 && beep)
+	{
+		// Search the next item that starts with the same letter
+		newPrefix = m_prefixSearch_prefix;
+		newPos = FindItemWithPrefix(newPrefix, item + 1);
+	}
+
+	m_prefixSearch_prefix = newPrefix;
+	if (newPos == -1)
+	{
+		if (beep)
+			wxBell();
+		return;
+	}
+
+	item = GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED);
+	while (item != -1)
+	{
+		SetItemState(item, 0, wxLIST_STATE_SELECTED | wxLIST_STATE_FOCUSED);
+		item = GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED);
+	}
+	SetItemState(newPos, wxLIST_STATE_SELECTED | wxLIST_STATE_FOCUSED, wxLIST_STATE_SELECTED | wxLIST_STATE_FOCUSED);
+	EnsureVisible(newPos);
+}
+
+void wxListCtrlEx::OnKeyDown(wxKeyEvent& event)
+{
+	if (!m_prefixSearch_enabled)
+	{
+		event.Skip();
+		return;
+	}
+
+#if defined(__WXMSW__) && wxUSE_UNICODE
+	wxChar key = MapVirtualKey(event.GetUnicodeKey(), 2);
+	if (key < 32)
+	{
+		event.Skip();
+		return;
+	}
+	else if (key == 32 || event.HasModifiers())
+	{
+		event.Skip();
+		return;
+	}
+	HandlePrefixSearch(key);
+	return;
+#else
+	int code = event.GetKeyCode();
+	if (code > 32 && code < 300 && !event.HasModifiers())
+	{
+#if wxUSE_UNICODE
+		int unicodeKey = event.GetUnicodeKey();
+		if (unicodeKey)
+			code = unicodeKey;
+#endif
+		HandlePrefixSearch(code);
+	}
+	else
+		event.Skip();
+#endif //defined(__WXMSW__) && wxUSE_UNICODE
+}
+
+// Declared const due to design error in wxWidgets.
+// Won't be fixed since a fix would break backwards compatibility
+// Both functions use a const_cast<CLocalListView *>(this) and modify
+// the instance.
+wxString wxListCtrlEx::OnGetItemText(long item, long column) const
+{
+	wxListCtrlEx *pThis = const_cast<wxListCtrlEx *>(this);
+	return pThis->GetItemText(item, (unsigned int)column);
+}
+
+int wxListCtrlEx::FindItemWithPrefix(const wxString& searchPrefix, int start)
+{
+	const int count = GetItemCount();
+	for (int i = start; i < (count + start); i++)
+	{
+		int item = i % count;
+		wxString namePrefix = GetItemText(item, 0).Left(searchPrefix.Length());
+		if (!namePrefix.CmpNoCase(searchPrefix))
+			return i % count;
+	}
+	return -1;
+}
---
+++ FileZilla3/tags/3.0.7/src/interface/recursive_operation.cpp
@@ -0,0 +1,294 @@
+#include "FileZilla.h"
+#include "recursive_operation.h"
+#include "commandqueue.h"
+#include "chmoddialog.h"
+#include "filter.h"
+#include "queue.h"
+
+CRecursiveOperation::CNewDir::CNewDir()
+{
+	recurse = true;
+}
+
+CRecursiveOperation::CRecursiveOperation(CState* pState)
+	: CStateEventHandler(pState, STATECHANGE_REMOTE_DIR),
+	  m_operationMode(recursive_none), m_pState(pState)
+{
+	m_pChmodDlg = 0;
+	m_pQueue = 0;
+}
+
+CRecursiveOperation::~CRecursiveOperation()
+{
+	if (m_pChmodDlg)
+	{
+		m_pChmodDlg->Destroy();
+		m_pChmodDlg = 0;
+	}
+}
+
+void CRecursiveOperation::OnStateChange(unsigned int event, const wxString& data)
+{
+	wxASSERT(m_pState);
+	if (event == STATECHANGE_REMOTE_DIR)
+		ProcessDirectoryListing(m_pState->GetRemoteDir());
+}
+
+void CRecursiveOperation::StartRecursiveOperation(enum OperationMode mode, const CServerPath& startDir, bool allowParent /*=false*/, const CServerPath& finalDir /*=CServerPath()*/)
+{
+	wxCHECK_RET(m_operationMode == recursive_none, _T("StartRecursiveOperation called with m_operationMode != recursive_none"));
+	wxCHECK_RET(m_pState->IsRemoteConnected(), _T("StartRecursiveOperation while disconnected"));
+	wxCHECK_RET(!startDir.IsEmpty(), _T("Empty startDir in StartRecursiveOperation"));
+
+	if (mode == recursive_chmod && !m_pChmodDlg)
+		return;
+
+	if ((mode == recursive_download || mode == recursive_addtoqueue) && !m_pQueue)
+		return;
+
+	if (m_dirsToVisit.empty())
+	{
+		// Nothing to do in this case
+		return;
+	}
+
+	m_operationMode = mode;
+
+	m_startDir = startDir;
+
+	if (finalDir.IsEmpty())
+		m_finalDir = startDir;
+	else
+		m_finalDir = finalDir;
+
+	m_allowParent = allowParent;
+
+	NextOperation();
+}
+
+void CRecursiveOperation::AddDirectoryToVisit(const CServerPath& path, const wxString& subdir, const wxString& localDir /*=_T("")*/)
+{
+	CNewDir dirToVisit;
+	dirToVisit.doVisit = true;
+	dirToVisit.localDir = localDir;
+	dirToVisit.parent = path;
+	dirToVisit.subdir = subdir;
+	m_dirsToVisit.push_back(dirToVisit);
+}
+
+void CRecursiveOperation::AddDirectoryToVisitRestricted(const CServerPath& path, const wxString& restrict, bool recurse)
+{
+	CNewDir dirToVisit;
+	dirToVisit.doVisit = true;
+	dirToVisit.parent = path;
+	dirToVisit.recurse = recurse;
+	dirToVisit.restrict = restrict;
+	m_dirsToVisit.push_back(dirToVisit);
+}
+
+bool CRecursiveOperation::NextOperation()
+{
+	if (m_operationMode == recursive_none)
+		return false;
+
+	while (!m_dirsToVisit.empty())
+	{
+		const CNewDir& dirToVisit = m_dirsToVisit.front();
+		if (m_operationMode == recursive_delete && !dirToVisit.doVisit)
+		{
+			m_pState->m_pCommandQueue->ProcessCommand(new CRemoveDirCommand(dirToVisit.parent, dirToVisit.subdir));
+			m_dirsToVisit.pop_front();
+			continue;
+		}
+
+		m_pState->m_pCommandQueue->ProcessCommand(new CListCommand(dirToVisit.parent, dirToVisit.subdir));
+		return true;
+	}
+
+	StopRecursiveOperation();
+	m_pState->m_pCommandQueue->ProcessCommand(new CListCommand(m_finalDir));
+	return false;
+}
+
+void CRecursiveOperation::ProcessDirectoryListing(const CDirectoryListing* pDirectoryListing)
+{
+	if (!pDirectoryListing)
+	{
+		StopRecursiveOperation();
+		return;
+	}
+
+	if (m_operationMode == recursive_none)
+		return;
+
+	wxASSERT(!m_dirsToVisit.empty());
+
+	if (!m_pState->IsRemoteConnected() || m_dirsToVisit.empty())
+	{
+		StopRecursiveOperation();
+		return;
+	}
+
+	CNewDir dir = m_dirsToVisit.front();
+	m_dirsToVisit.pop_front();
+
+	if (!pDirectoryListing->path.IsSubdirOf(m_startDir, false))
+	{
+		// In some cases (chmod from tree for example) it is neccessary to list the
+		// parent first
+		if (pDirectoryListing->path != m_startDir || !m_allowParent)
+		{
+			NextOperation();
+			return;
+		}
+	}
+
+	if (m_operationMode == recursive_delete && dir.doVisit && dir.subdir != _T(""))
+	{
+		// After recursing into directory to delete its contents, delete directory itself
+		// Gets handled in NextOperation
+		CNewDir dir2 = dir;
+		dir2.doVisit = false;
+		m_dirsToVisit.push_front(dir2);
+	}
+
+	// Check if we have already visited the directory
+	for (std::list<CServerPath>::const_iterator iter = m_visitedDirs.begin(); iter != m_visitedDirs.end(); iter++)
+	{
+		if (*iter == pDirectoryListing->path)
+		{
+			NextOperation();
+			return;
+		}
+	}
+	m_visitedDirs.push_back(pDirectoryListing->path);
+
+	const CServer* pServer = m_pState->GetServer();
+	wxASSERT(pServer);
+
+	if (!pDirectoryListing->GetCount())
+	{
+		wxFileName fn(dir.localDir, _T(""));
+		if (m_operationMode == recursive_download)
+		{
+			wxFileName::Mkdir(fn.GetPath(), 0777, wxPATH_MKDIR_FULL);
+			m_pState->RefreshLocalFile(fn.GetFullPath());
+		}
+		else if (m_operationMode == recursive_addtoqueue)
+			m_pQueue->QueueFile(true, true, fn.GetFullPath(), _T(""), CServerPath(), *pServer, -1);
+	}
+
+	CFilterDialog filter;
+
+	// Is operation restricted to a single child?
+	bool restrict = !dir.restrict.IsEmpty();
+
+	std::list<wxString> filesToDelete;
+	
+	for (int i = pDirectoryListing->GetCount() - 1; i >= 0; i--)
+	{
+		const CDirentry& entry = (*pDirectoryListing)[i];
+
+		if (restrict)
+		{
+			if (entry.name != dir.restrict)
+				continue;
+		}
+		else if (filter.FilenameFiltered(entry.name, entry.dir, entry.size, false, 0))
+			continue;
+
+		if (entry.dir && !entry.link)
+		{
+			if (dir.recurse)
+			{
+				CNewDir dirToVisit;
+				wxFileName fn(dir.localDir, _T(""));
+				fn.AppendDir(entry.name);
+				dirToVisit.parent = pDirectoryListing->path;
+				dirToVisit.subdir = entry.name;
+				dirToVisit.localDir = fn.GetFullPath();
+				dirToVisit.doVisit = true;
+				m_dirsToVisit.push_front(dirToVisit);
+			}
+		}
+		else
+		{
+			switch (m_operationMode)
+			{
+			case recursive_addtoqueue:
+			case recursive_download:
+				{
+					wxFileName fn(dir.localDir, entry.name);
+					m_pQueue->QueueFile(m_operationMode == recursive_addtoqueue, true, fn.GetFullPath(), entry.name, pDirectoryListing->path, *pServer, entry.size);
+				}
+				break;
+			case recursive_delete:
+				filesToDelete.push_back(entry.name);
+				break;
+			default:
+				break;
+			}
+		}
+
+		if (m_operationMode == recursive_chmod && m_pChmodDlg)
+		{
+			const int applyType = m_pChmodDlg->GetApplyType();
+			if (!applyType ||
+				(!entry.dir && applyType == 1) ||
+				(entry.dir && applyType == 2))
+			{
+				char permissions[9];
+				bool res = m_pChmodDlg->ConvertPermissions(entry.permissions, permissions);
+				wxString newPerms = m_pChmodDlg->GetPermissions(res ? permissions : 0);
+				m_pState->m_pCommandQueue->ProcessCommand(new CChmodCommand(pDirectoryListing->path, entry.name, newPerms));
+			}
+		}
+	}
+
+	if (m_operationMode == recursive_delete && !filesToDelete.empty())
+		m_pState->m_pCommandQueue->ProcessCommand(new CDeleteCommand(pDirectoryListing->path, filesToDelete));
+
+	NextOperation();
+}
+
+void CRecursiveOperation::SetChmodDialog(CChmodDialog* pChmodDialog)
+{
+	wxASSERT(pChmodDialog);
+
+	if (m_pChmodDlg)
+		m_pChmodDlg->Destroy();
+
+	m_pChmodDlg = pChmodDialog;
+}
+
+void CRecursiveOperation::StopRecursiveOperation()
+{
+	m_operationMode = recursive_none;
+	m_dirsToVisit.clear();
+	m_visitedDirs.clear();
+
+	if (m_pChmodDlg)
+	{
+		m_pChmodDlg->Destroy();
+		m_pChmodDlg = 0;
+	}
+}
+
+void CRecursiveOperation::ListingFailed()
+{
+	if (m_operationMode == recursive_none)
+		return;
+
+	wxASSERT(!m_dirsToVisit.empty());
+	if (m_dirsToVisit.empty())
+		return;
+
+	m_dirsToVisit.pop_front();
+
+	NextOperation();
+}
+
+void CRecursiveOperation::SetQueue(CQueueView* pQueue)
+{
+	m_pQueue = pQueue;
+}

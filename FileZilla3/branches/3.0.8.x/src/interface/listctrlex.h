2b7b1653472107ddbc7a78aa1ccdcea928c1788c
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2008-03-17 19:11:11 +0000

    Branch 3.0.8.x

---
+++ FileZilla3/branches/3.0.8.x/src/interface/listctrlex.h
@@ -0,0 +1,57 @@
+#ifndef __LISTCTRLEX_H__
+#define __LISTCTRLEX_H__
+
+class wxListCtrlEx : public wxListCtrl
+{
+public:
+	wxListCtrlEx(wxWindow *parent,
+		wxWindowID id = wxID_ANY,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize,
+		long style = wxLC_ICON,
+		const wxValidator& validator = wxDefaultValidator,
+		const wxString& name = wxListCtrlNameStr);
+	~wxListCtrlEx();
+
+	// Ensure that the given item is the first in the list
+	void ScrollTopItem(int item);
+
+	void EnablePrefixSearch(bool enable) { m_prefixSearch_enabled = enable; }
+
+	void SaveSetItemCount(long count);
+
+protected:
+	virtual void OnPostScroll();
+	virtual void OnPreEmitPostScrollEvent();
+	void EmitPostScrollEvent();
+
+	virtual wxString GetItemText(int item, unsigned int column) { return _T(""); }
+
+	virtual wxString OnGetItemText(long item, long column) const;
+
+#ifdef __WXGTK__
+	wxBitmap GetSortArrow(bool sortDown);
+#endif
+
+private:
+	// Keyboard prefix search
+	void HandlePrefixSearch(wxChar character);
+	int FindItemWithPrefix(const wxString& searchPrefix, int start);
+
+#ifndef __WXMSW__
+	wxScrolledWindow* GetMainWindow();
+#endif
+
+	DECLARE_EVENT_TABLE();
+	void OnPostScrollEvent(wxCommandEvent& event);
+	void OnScrollEvent(wxScrollWinEvent& event);
+	void OnMouseWheel(wxMouseEvent& event);
+	void OnSelectionChanged(wxListEvent& event);
+	void OnKeyDown(wxKeyEvent& event);
+
+	bool m_prefixSearch_enabled;
+	wxDateTime m_prefixSearch_lastKeyPress;
+	wxString m_prefixSearch_prefix;
+};
+
+#endif //__LISTCTRLEX_H__
---
+++ FileZilla3/branches/3.0.8.x/src/interface/statuslinectrl.h
@@ -0,0 +1,50 @@
+#ifndef __STATUSLINECTRL_H__
+#define __STATUSLINECTRL_H__
+
+class CQueueView;
+class CStatusLineCtrl : public wxWindow
+{
+public:
+	CStatusLineCtrl(CQueueView* pParent, const t_EngineData* const pEngineData, const wxRect& initialPosition);
+	~CStatusLineCtrl();
+
+	const CFileItem* GetItem() const { return m_pEngineData->pItem; }
+
+	void SetEngineData(const t_EngineData* const pEngineData);
+
+	void SetTransferStatus(const CTransferStatus* pStatus);
+	wxLongLong GetLastOffset() const { return m_pStatus ? m_pStatus->currentOffset : m_lastOffset; }
+	wxLongLong GetTotalSize() const { return m_pStatus ? m_pStatus->totalSize : -1; }
+	wxLongLong GetSpeed() const;
+
+	// Returns true if some progress has been made since transfer started
+	bool MadeProgress() const { return m_madeProgress; }
+
+	virtual bool Show(bool show = true);
+
+protected:
+	void DrawRightAlignedText(wxDC& dc, wxString text, int x, int y);
+	void DrawProgressBar(wxDC& dc, int x, int y, int height);
+
+	CQueueView* m_pParent;
+	const t_EngineData* m_pEngineData;
+	CTransferStatus* m_pStatus;
+
+	wxString m_statusText;
+	wxTimer m_transferStatusTimer;
+
+	static int m_fieldOffsets[4];
+	static wxCoord m_textHeight;
+	static bool m_initialized;
+
+	bool m_madeProgress;
+
+	wxLongLong m_lastOffset; // Stores the last transfer offset so that the total queue size can be accurately calculated.
+
+	DECLARE_EVENT_TABLE()
+	void OnPaint(wxPaintEvent& event);
+	void OnTimer(wxTimerEvent& event);
+	void OnEraseBackground(wxEraseEvent& event);
+};
+
+#endif // __STATUSLINECTRL_H__

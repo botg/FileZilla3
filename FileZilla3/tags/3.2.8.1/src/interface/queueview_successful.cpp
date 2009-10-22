7c98db7035235ba1cdd4a8794ebf1899dc3f8eb5
botg <botg@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2009-10-22 09:29:19 +0000

    Tag 3.2.8.1

---
+++ FileZilla3/tags/3.2.8.1/src/interface/queueview_successful.cpp
@@ -0,0 +1,45 @@
+#include "FileZilla.h"
+#include "queue.h"
+#include "queueview_successful.h"
+#include "Options.h"
+
+BEGIN_EVENT_TABLE(CQueueViewSuccessful, CQueueViewFailed)
+EVT_CONTEXT_MENU(CQueueViewSuccessful::OnContextMenu)
+EVT_MENU(XRCID("ID_AUTOCLEAR"), CQueueViewSuccessful::OnMenuAutoClear)
+END_EVENT_TABLE()
+
+CQueueViewSuccessful::CQueueViewSuccessful(CQueue* parent, int index)
+	: CQueueViewFailed(parent, index, _("Successful transfers"))
+{
+	CreateColumns();
+
+	m_autoClear = COptions::Get()->GetOptionVal(OPTION_QUEUE_SUCCESSFUL_AUTOCLEAR) ? true : false;
+}
+
+void CQueueViewSuccessful::OnContextMenu(wxContextMenuEvent& event)
+{
+	wxMenu* pMenu = wxXmlResource::Get()->LoadMenu(_T("ID_MENU_QUEUE_SUCCESSFUL"));
+	if (!pMenu)
+		return;
+
+#ifndef __WXMSW__
+	// GetNextItem is O(n) if nothing is selected, GetSelectedItemCount() is O(1)
+	const bool has_selection = GetSelectedItemCount() != 0;
+#else
+	const bool has_selection = GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED) != -1;
+#endif
+
+	pMenu->Enable(XRCID("ID_REMOVE"), has_selection);
+	pMenu->Enable(XRCID("ID_REQUEUE"), has_selection);
+	pMenu->Check(XRCID("ID_AUTOCLEAR"), m_autoClear);
+
+	PopupMenu(pMenu);
+
+	delete pMenu;
+}
+
+void CQueueViewSuccessful::OnMenuAutoClear(wxCommandEvent& event)
+{
+	m_autoClear = !m_autoClear;
+	COptions::Get()->SetOption(OPTION_QUEUE_SUCCESSFUL_AUTOCLEAR, m_autoClear ? true : false);
+}

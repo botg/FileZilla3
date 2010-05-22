9aff769c59e43015eaa65258042d1a9e5e3fe0d4
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2010-05-22 16:54:16 +0000

    * Start branch for wx3 porting effforts

---
+++ FileZilla3/branches/wx3/src/interface/aui_notebook_ex.h
@@ -0,0 +1,43 @@
+#ifndef __AUI_NOTEBOOK_EX_H__
+#define __AUI_NOTEBOOK_EX_H__
+
+#include <wx/aui/aui.h>
+
+class wxAuiTabArtEx;
+class wxAuiNotebookEx : public wxAuiNotebook
+{
+public:
+	wxAuiNotebookEx();
+	virtual ~wxAuiNotebookEx();
+
+	void RemoveExtraBorders();
+
+	void SetExArtProvider();
+
+	// Basically identical to the AUI one, but not calling Update
+	bool SetPageText(size_t page_idx, const wxString& text);
+
+	void Highlight(size_t page, bool highlight = true);
+	bool Highlighted(size_t page) const;
+
+	void OnNavigationKey(wxNavigationKeyEvent& event);
+
+	void AdvanceTab(bool forward);
+
+#ifdef __WXGTK__
+	virtual bool SetCursor(const wxCursor& cursor);
+#endif
+protected:
+	std::vector<bool> m_highlighted;
+
+	DECLARE_EVENT_TABLE()
+	void OnPageChanged(wxAuiNotebookEvent& event);
+
+#ifdef __WXGTK__
+	void OnSetCursor(wxSetCursorEvent& event);
+
+	bool m_ignore_next_set_cursor;
+#endif
+};
+
+#endif //__AUI_NOTEBOOK_EX_H__

2b3f6658b327d26fe96933e9e6298448c5a4542a
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2010-09-04 21:24:03 +0000

    Tag 3.2.7-rc1

---
+++ FileZilla3/tags/3.2.7-rc1/src/interface/power_management.h
@@ -0,0 +1,45 @@
+#ifndef __POWER_MANAGEMENT_H__
+#define __POWER_MANAGEMENT_H__
+
+// This simple class prevents the system from entering idle sleep
+// while FileZilla is busy
+
+#include "state.h"
+#ifdef __WXMAC__
+#include <IOKit/pwr_mgt/IOPMLib.h>
+#endif
+
+class CMainFrame;
+#ifdef WITH_LIBDBUS
+class CPowerManagementInhibitor;
+#endif
+
+class CPowerManagement : protected CStateEventHandler
+{
+public:
+	static void Create(CMainFrame* pMainFrame);
+	static void Destroy();
+
+protected:
+	CPowerManagement(CMainFrame* pMainFrame);
+	virtual ~CPowerManagement();
+	
+	static CPowerManagement* m_pPowerManagement;
+
+	virtual void OnStateChange(CState* pState, enum t_statechange_notifications notification, const wxString& data, const void* data2);
+
+	void DoSetBusy();
+	void DoSetIdle();
+
+	bool m_busy;
+	
+	CMainFrame* m_pMainFrame;
+
+#ifdef WITH_LIBDBUS
+	CPowerManagementInhibitor *m_inhibitor;
+#elif defined(__WXMAC__)
+	IOPMAssertionID m_assertionID;
+#endif
+};
+
+#endif //__POWER_MANAGEMENT_H__

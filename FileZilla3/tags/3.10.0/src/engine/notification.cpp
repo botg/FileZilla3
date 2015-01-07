f5d2a41a429e8d02a377eaf697c4f4c48205a30d
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2015-01-07 13:15:39 +0000

    Tag 3.10.0

---
+++ FileZilla3/tags/3.10.0/src/engine/notification.cpp
@@ -0,0 +1,241 @@
+#include <filezilla.h>
+
+wxDEFINE_EVENT(fzEVT_NOTIFICATION, wxFzEvent);
+
+wxFzEvent::wxFzEvent()
+	: wxEvent(wxID_ANY, fzEVT_NOTIFICATION)
+	, engine_()
+{
+}
+
+wxFzEvent::wxFzEvent(CFileZillaEngine const* engine)
+	: wxEvent(wxID_ANY, fzEVT_NOTIFICATION)
+	, engine_(engine)
+{
+}
+
+wxFzEvent::wxFzEvent(int id)
+	: wxEvent(id, fzEVT_NOTIFICATION)
+	, engine_()
+{
+}
+
+wxEvent *wxFzEvent::Clone() const
+{
+	return new wxFzEvent(*this);
+}
+
+CDirectoryListingNotification::CDirectoryListingNotification(const CServerPath& path, const bool modified /*=false*/, const bool failed /*=false*/)
+	: m_modified(modified), m_failed(failed), m_path(path)
+{
+}
+
+enum RequestId CFileExistsNotification::GetRequestID() const
+{
+	return reqId_fileexists;
+}
+
+CInteractiveLoginNotification::CInteractiveLoginNotification(const wxString& challenge)
+	: m_challenge(challenge)
+{
+}
+
+enum RequestId CInteractiveLoginNotification::GetRequestID() const
+{
+	return reqId_interactiveLogin;
+}
+
+CActiveNotification::CActiveNotification(int direction)
+	: m_direction(direction)
+{
+}
+
+CTransferStatusNotification::CTransferStatusNotification(CTransferStatus *pStatus)
+	: m_pStatus(pStatus)
+{
+}
+
+CTransferStatusNotification::~CTransferStatusNotification()
+{
+	delete m_pStatus;
+}
+
+const CTransferStatus* CTransferStatusNotification::GetStatus() const
+{
+	return m_pStatus;
+}
+
+CHostKeyNotification::CHostKeyNotification(wxString host, int port, wxString fingerprint, bool changed /*=false*/)
+	: m_host(host), m_port(port), m_fingerprint(fingerprint), m_changed(changed)
+{
+}
+
+enum RequestId CHostKeyNotification::GetRequestID() const
+{
+	return m_changed ? reqId_hostkeyChanged : reqId_hostkey;
+}
+
+wxString CHostKeyNotification::GetHost() const
+{
+	return m_host;
+}
+
+int CHostKeyNotification::GetPort() const
+{
+	return m_port;
+}
+
+wxString CHostKeyNotification::GetFingerprint() const
+{
+	return m_fingerprint;
+}
+
+CDataNotification::CDataNotification(char* pData, int len)
+	: m_pData(pData), m_len(len)
+{
+}
+
+CDataNotification::~CDataNotification()
+{
+	delete [] m_pData;
+}
+
+char* CDataNotification::Detach(int& len)
+{
+	len = m_len;
+	char* pData = m_pData;
+	m_pData = 0;
+	return pData;
+}
+
+CCertificate::CCertificate(
+		const unsigned char* rawData, unsigned int len,
+		wxDateTime activationTime, wxDateTime expirationTime,
+		const wxString& serial,
+		const wxString& pkalgoname, unsigned int bits,
+		const wxString& signalgoname,
+		const wxString& fingerprint_md5,
+		const wxString& fingerprint_sha1,
+		const wxString& subject,
+		const wxString& issuer)
+{
+	wxASSERT(len);
+	if (len)
+	{
+		m_rawData = new unsigned char[len];
+		memcpy(m_rawData, rawData, len);
+	}
+	else
+		m_rawData = 0;
+	m_len = len;
+
+	m_activationTime = activationTime;
+	m_expirationTime = expirationTime;
+
+	m_serial = serial;
+	m_pkalgoname = pkalgoname;
+	m_pkalgobits = bits;
+
+	m_signalgoname = signalgoname;
+
+	m_fingerprint_md5 = fingerprint_md5;
+	m_fingerprint_sha1 = fingerprint_sha1;
+
+	m_subject = subject;
+	m_issuer = issuer;
+}
+
+CCertificate::CCertificate(const CCertificate &op)
+{
+	if (op.m_rawData)
+	{
+		wxASSERT(op.m_len);
+		if (op.m_len)
+		{
+			m_rawData = new unsigned char[op.m_len];
+			memcpy(m_rawData, op.m_rawData, op.m_len);
+		}
+		else
+			m_rawData = 0;
+	}
+	else
+		m_rawData = 0;
+	m_len = op.m_len;
+
+	m_activationTime = op.m_activationTime;
+	m_expirationTime = op.m_expirationTime;
+
+	m_serial = op.m_serial;
+	m_pkalgoname = op.m_pkalgoname;
+	m_pkalgobits = op.m_pkalgobits;
+
+	m_signalgoname = op.m_signalgoname;
+
+	m_fingerprint_md5 = op.m_fingerprint_md5;
+	m_fingerprint_sha1 = op.m_fingerprint_sha1;
+
+	m_subject = op.m_subject;
+	m_issuer = op.m_issuer;
+}
+
+CCertificate::~CCertificate()
+{
+	delete [] m_rawData;
+}
+
+CCertificate& CCertificate::operator=(const CCertificate &op)
+{
+	if (&op == this)
+		return *this;
+
+	delete [] m_rawData;
+	if (op.m_rawData)
+	{
+		wxASSERT(op.m_len);
+		if (op.m_len)
+		{
+			m_rawData = new unsigned char[op.m_len];
+			memcpy(m_rawData, op.m_rawData, op.m_len);
+		}
+		else
+			m_rawData = 0;
+	}
+	else
+		m_rawData = 0;
+	m_len = op.m_len;
+
+	m_activationTime = op.m_activationTime;
+	m_expirationTime = op.m_expirationTime;
+
+	m_serial = op.m_serial;
+	m_pkalgoname = op.m_pkalgoname;
+	m_pkalgobits = op.m_pkalgobits;
+
+	m_signalgoname = op.m_signalgoname;
+
+	m_fingerprint_md5 = op.m_fingerprint_md5;
+	m_fingerprint_sha1 = op.m_fingerprint_sha1;
+
+	m_subject = op.m_subject;
+	m_issuer = op.m_issuer;
+
+	return *this;
+}
+
+CCertificateNotification::CCertificateNotification(const wxString& host, unsigned int port,
+		const wxString& protocol,
+		const wxString& keyExchange,
+		const wxString& sessionCipher,
+		const wxString& sessionMac,
+		const std::vector<CCertificate> &certificates)
+	: m_protocol( protocol )
+	, m_keyExchange( keyExchange )
+{
+	m_host = host;
+	m_port = port;
+
+	m_sessionCipher = sessionCipher;
+	m_sessionMac = sessionMac;
+
+	m_certificates = certificates;
+}
---
+++ FileZilla3/tags/3.10.0/src/interface/statuslinectrl.h
@@ -0,0 +1,75 @@
+#ifndef __STATUSLINECTRL_H__
+#define __STATUSLINECTRL_H__
+
+class CQueueView;
+class CStatusLineCtrl final : public wxWindow
+{
+public:
+	CStatusLineCtrl(CQueueView* pParent, const t_EngineData* const pEngineData, const wxRect& initialPosition);
+	~CStatusLineCtrl();
+
+	const CFileItem* GetItem() const { return m_pEngineData->pItem; }
+
+	void SetEngineData(const t_EngineData* const pEngineData);
+
+	void SetTransferStatus(const CTransferStatus* pStatus);
+	wxLongLong GetLastOffset() const { return status_valid_ ? status_.currentOffset : m_lastOffset; }
+	wxLongLong GetTotalSize() const { return status_valid_ ? status_.totalSize : -1; }
+	wxFileOffset GetSpeed(int elapsed_milli_seconds);
+	wxFileOffset GetCurrentSpeed();
+
+	virtual bool Show(bool show = true);
+
+protected:
+	void InitFieldOffsets();
+
+	void DrawRightAlignedText(wxDC& dc, wxString text, int x, int y);
+	void DrawProgressBar(wxDC& dc, int x, int y, int height, int bar_split, int permill);
+
+	CQueueView* m_pParent;
+	const t_EngineData* m_pEngineData;
+	CTransferStatus status_;
+	bool status_valid_{};
+
+	wxString m_statusText;
+	wxTimer m_transferStatusTimer;
+
+	static int m_fieldOffsets[4];
+	static wxCoord m_textHeight;
+	static bool m_initialized;
+
+	bool m_madeProgress;
+
+	wxLongLong m_lastOffset{-1}; // Stores the last transfer offset so that the total queue size can be accurately calculated.
+
+	// This is used by GetSpeed to forget about the first 10 seconds on longer transfers
+	// since at the very start the speed is hardly accurate (e.g. due to TCP slow start)
+	struct _past_data final
+	{
+		int elapsed{};
+		wxFileOffset offset{};
+	} m_past_data[10];
+	int m_past_data_count{};
+
+	//Used by getCurrentSpeed
+	wxDateTime m_gcLastTimeStamp;
+	wxFileOffset m_gcLastOffset{-1};
+	wxFileOffset m_gcLastSpeed{-1};
+
+	//Used to avoid excessive redraws
+	wxBitmap m_data;
+	std::unique_ptr<wxMemoryDC> m_mdc;
+	wxString m_previousStatusText;
+	int m_last_elapsed_seconds{};
+	int m_last_left{};
+	wxString m_last_bytes_and_rate;
+	int m_last_bar_split{-1};
+	int m_last_permill{-1};
+
+	DECLARE_EVENT_TABLE()
+	void OnPaint(wxPaintEvent& event);
+	void OnTimer(wxTimerEvent& event);
+	void OnEraseBackground(wxEraseEvent& event);
+};
+
+#endif // __STATUSLINECTRL_H__
